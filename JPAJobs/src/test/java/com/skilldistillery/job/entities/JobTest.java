7d24cd30967f50d6ec024f345e7a2d4584a1413c
Andy Cary <info@andycary.com>
2022-04-16 21:37:02 -0400

    stub entity, configure hibernate

---
+++ JPAJobs/src/test/java/com/skilldistillery/job/entities/JobTest.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.job.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JobTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Job job;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAJobs");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		job = em.find(Job.class, 1);	
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		emf.close();
+		job = null;
+	}
+
+	@Test
+	void test_Job_entity_mapping() {
+		assertNotNull(job);
+		assertEquals("Software Engineer", job.getName());
+	}
+
+}
