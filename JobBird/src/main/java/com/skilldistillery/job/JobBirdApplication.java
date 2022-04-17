e22e4ea5bf40a13833cccca886d1b05e2e9b0f05
Andy Cary <info@andycary.com>
2022-04-16 21:58:31 -0400

    Add Spring Boot and JPA project dependency

---
+++ JobBird/src/main/java/com/skilldistillery/job/JobBirdApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBirdApplication.class, args);
+	}
+
+}
